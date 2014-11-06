7177a934a17ba375d5b44aa793e7b9910369300a
Roger Stringer <roger@freekrai.net>
2014-11-06 14:53:08 -0800

    settings-default.php

---
+++ settings-default.php
@@ -0,0 +1,9 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    define("READY",true);
+    $accountsid = 'ACCOUNT SID HERE';
+    $authtoken = 'AUTH TOKEN HERE';
+    $fromnumber = 'FROM NUMBER HERE';
+	$siteurl = 'http://rser.me/wb/';
+?>
