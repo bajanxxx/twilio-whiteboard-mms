eed5ec708b31aadd6e5ffa40f6fb7f4f9e61eeb8
Roger Stringer <roger@freekrai.net>
2014-11-09 09:27:36 -0800

    Update settings-default.php

--- settings-default.php
+++ settings-default.php
@@ -1,9 +1,9 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', '1');
-    define("READY",true);
-    $accountsid = 'ACCOUNT SID HERE';
-    $authtoken = 'AUTH TOKEN HERE';
-    $fromnumber = 'FROM NUMBER HERE';
-	$siteurl = 'http://rser.me/wb/';
+	error_reporting(0);
+	@ini_set('display_errors', 0);
+	define("READY",true);
+	$accountsid = 'ACCOUNT SID HERE';
+	$authtoken = 'AUTH TOKEN HERE';
+	$fromnumber = 'FROM NUMBER HERE';
+	$siteurl = 'URL WHERE APP IS INSTALLED';
 ?>
